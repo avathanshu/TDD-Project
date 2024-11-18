7b8d00dd0f4058d2a31d8b0402f829aec12b2392
Avathanshu <avathanshu@gmail.com>
2024-11-18 22:30:06 +1300

    renamed

--- functions.test.js
+++ functions.test.js
@@ -1,4 +1,4 @@
-const capitalize = require('./capitalize');
+const capitalize = require('./functions');
 
 test('capitalizes the first letter of a string', () => {
     expect(capitalize("hello")).toBe("Hello")
