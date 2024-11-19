1069036bcf7031bc0eef708b74c1ba2174434aec
Avathanshu <avathanshu@gmail.com>
2024-11-20 12:50:07 +1300

    capitalize, reverseString, and calculator testing

--- functions.js
+++ functions.js
@@ -4,6 +4,37 @@ function capitalize(str) {
     return newString;
 }
 
+function reverseString(str) {
+    let input = str;
+    let newString = "";
+    for (let i = 0; i < input.length; i++) {
+        newString += input[input.length - i - 1];
+    }
+    return newString;
+}
 
-capitalize("hello")
-module.exports = capitalize;
+function calculator() {
+    function add(a,b) {
+        return a + b;
+    }
+
+    function subtract(a,b) {
+        return a - b;
+    }
+
+    function multiply(a,b) {
+        return a * b;
+    }
+
+    function divide(a,b) {
+        return (a / b);
+    }
+
+    return { add, subtract, multiply, divide }
+}
+
+
+
+
+
+module.exports = { capitalize, reverseString, calculator };
--- functions.test.js
+++ functions.test.js
@@ -1,5 +1,17 @@
-const capitalize = require('./functions');
+const { capitalize, reverseString, calculator } = require('./functions');
+
 
 test('capitalizes the first letter of a string', () => {
-    expect(capitalize("hello")).toBe("Hello")
+    expect(capitalize("hello")).toBe("Hello");
+})
+
+test("Reverses the string", () => {
+    expect(reverseString("Hello")).toEqual("olleH");
+})
+
+test("Calcuator", () => {
+    expect(calculator().add(1,2)).toBe(3);
+    expect(calculator().subtract(1,2)).toBe(-1);
+    expect(calculator().multiply(1,2)).toBe(2);
+    expect(calculator().divide(1,2)).toBe(0.5);
 })
